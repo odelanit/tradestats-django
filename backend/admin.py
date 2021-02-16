1e0c9c8a9817b12a4aa1fe217e13d1ed017ce12e
webdev0101 <hesimiao2019@gmail.com>
2021-02-17 02:19:09 +0800

    added index constituent

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from backend.models import InstrumentMaster, InstrumentMasterAdmin
+from backend.models import InstrumentMaster, InstrumentMasterAdmin, IndexConstituent, IndexConstituentAdmin
 
 admin.site.register(InstrumentMaster, InstrumentMasterAdmin)
+admin.site.register(IndexConstituent, IndexConstituentAdmin)
