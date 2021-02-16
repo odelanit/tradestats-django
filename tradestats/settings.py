0138ffa79322bfc63ecaa70621536ee0d2f8b5cf
webdev0101 <hesimiao2019@gmail.com>
2021-02-17 01:49:37 +0800

    fetch instrument master

--- tradestats/settings.py
+++ tradestats/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p!f1&9%b%_&n_t5f#0o07$$+msi)4m1lae%kks1#t!^o$5!o20'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'secretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
